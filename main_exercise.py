d0338fd9c578621fa66c2fcf17b20249709a6145
Soumya Banerjee <neel.soumya@gmail.com>
2024-12-04 13:01:04 +0000

    introducing some vey basic chain of thought prompting

--- main_exercise.py
+++ main_exercise.py
@@ -5,6 +5,9 @@ from rich import print as rprint
 
 dotenv.load_dotenv()
 
+# Global parameters
+MAX_NUM_TOKENS_RESPONSE = 1000
+
 # API call
 client = OpenAI()
 
@@ -23,4 +26,24 @@ response = client.chat.completions.create(
     max_tokens=128
 ) 
 
+print("Simple prompt \n")
 print(response.choices[0].message.content)
+print("*******************************************\n")
+
+# give it prompt v2
+system_prompt = "You are an expert on computational fluid dynamics and gas turbine and ducted fan design"
+user_query = "Can you please tell me how to optimize efficiency of a ducted fan for a drone? Assume you have access to the diffusion factor for flow through the turbine. Please step through your reasoning and explain all the steps in your reasoning. For example, start byd efining efficiency, then determine how diffusion factor will affect efficiency, then determine how to change diffusion factor. Please provide references and links to justify your arguments."
+
+response = client.chat.completions.create(
+    model= "gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "system", "content": user_query},
+    ],
+    max_tokens=MAX_NUM_TOKENS_RESPONSE
+) 
+
+print("*******************************************\n")
+print("Chain of thought prompting \n")
+print(response.choices[0].message.content)
+print("*******************************************\n")
