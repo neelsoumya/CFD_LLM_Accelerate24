c54668af59e71bb91d6b11d5faa306cc4fbfff3e
Soumya Banerjee <neel.soumya@gmail.com>
2024-12-04 18:18:16 +0000

    prototyping

--- main_exercise.py
+++ main_exercise.py
@@ -21,7 +21,7 @@ response = client.chat.completions.create(
     model= "gpt-4o-mini",
     messages=[
         {"role": "system", "content": system_prompt},
-        {"role": "system", "content": user_query},
+        {"role": "user", "content": user_query},
     ],
     max_tokens=128
 ) 
@@ -40,7 +40,7 @@ response = client.chat.completions.create(
     model= "gpt-4o-mini",
     messages=[
         {"role": "system", "content": system_prompt},
-        {"role": "system", "content": user_query},
+        {"role": "user", "content": user_query},
     ],
     max_tokens=MAX_NUM_TOKENS_RESPONSE
 ) 
@@ -48,4 +48,27 @@ response = client.chat.completions.create(
 print("*******************************************\n")
 print("Chain of thought prompting \n")
 print(response.choices[0].message.content)
+print("\n*******************************************\n")
+
+
+print("More complex prompt on automated scientific discovery \n")
 print("*******************************************\n")
+
+# prompt on automated scientific discovery
+system_prompt = "You are an expert on scientific discovery. You are tasked with finding the relationship between variables. "
+user_query = "You are given the values of variables in a tabular format. Can you please tell me how to optimize efficiency of a ducted fan for a drone? Assume you have access to the diffusion factor for flow through the turbine. Please step through your reasoning and explain all the steps in your reasoning. For example, start byd efining efficiency, then determine how diffusion factor will affect efficiency, then determine how to change diffusion factor. Please provide tell me the relationship between these variables? The values are given in a table in latex code here: \begin{tabular}{|c c c c c c c|} \hline Planet & Distance ($D$) & Period ($P$) & $\frac{D}{P}$ & $\frac{D^2}{P}$ & $\frac{D^2}{P^2}$ & $\frac{D^3}{P^2}$ \\\hline $A$ & 1.0 & 1.0 & 1.0 & 1.0 & 1.0 & 1.0 \\ $B$ & 4.0 & 8.0 & 0.5 & 2.0 & 0.25 & 1.0 \\ $C$ & 9.0 & 27.0 & 0.333 & 3.0 & 0.111 & 1.0 \\ \hline \end{tabular} "
+
+response = client.chat.completions.create(
+    model= "gpt-4o-mini",
+    messages=[
+        {"role":"system", "content": system_prompt},
+        {"role":"user", "content": user_query},
+    ],
+    max_tokens=MAX_NUM_TOKENS_RESPONSE
+)
+
+
+print("*******************************************\n")
+print("Chain of thought prompting \n")
+print(response.choices[0].message.content)
+print("\n*******************************************\n")
