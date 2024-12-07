b3038e67c214641b85488de30e19fd16dce000cf
Soumya Banerjee <neel.soumya@gmail.com>
2024-12-07 11:19:36 +0000

    tested open source LLM code

--- opensource_llm.py
+++ opensource_llm.py
@@ -1,12 +1,12 @@
 from transformers import pipeline
 
 # load an open source LLM
-generator = pipeline("text-generation", model="gpt-2")
+generator = pipeline("text-generation", model="gpt2")
 
 # give prompt
-str_prompt = "Once upon a time"
+str_prompt = "What is a transformer model? Explain like I am five."
 
-output = generator(str_prompt, max_length = 100, num_return_sequences = 1)
+output = generator(str_prompt, max_length = 1000, num_return_sequences = 1)
 
 # print
 print(output[0]["generated_text"])
