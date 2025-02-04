8f0d638135c10a1d4a57cdc55e62c263b6ec6ee1
Soumya Banerjee <neel.soumya@gmail.com>
2025-02-04 16:50:00 +0000

    Update opensource_llm.py

--- opensource_llm.py
+++ opensource_llm.py
@@ -11,3 +11,8 @@ output = generator(str_prompt, max_length = 1000, num_return_sequences = 1)
 # print
 print(output[0]["generated_text"])
 
+# TODO:
+# https://huggingface.co/meta-llama/Llama-3.2-1B
+# https://github.com/acceleratescience/large-language-models/blob/main/notebooks/finetuning.ipynb
+# https://huggingface.co/pico-lm
+
